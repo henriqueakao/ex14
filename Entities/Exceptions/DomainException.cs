8c7d04e623eee142d78c0e2844cf165cd770ef12
Henrique Akao <henriquetanioka@gmail.com>
2020-08-09 01:46:53 -0300

    v1

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ex14.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
